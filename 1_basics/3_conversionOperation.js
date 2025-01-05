c0b2ff232563b366dc271c0f54730f7962d203df
maheshshinde16 <maheshshindep12@gmail.com>
2025-01-05 02:07:16 +0000

    conversion of datatypes

---
+++ 1_basics/3_conversionOperation.js
@@ -0,0 +1,32 @@
+// let score = "3a3"
+// let score = null
+// let score = undefined
+let score = "mahesh"
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// "33" = 33
+// "33dd" = NaN
+// true = 1; false = 0
+
+let isLoggedIn = "mahesh"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1= true; 0= false 
+// ""=false
+// "mahesh"= true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
